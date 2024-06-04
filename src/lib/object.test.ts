c38fef55f5647ebfd227865cb5fdc42ec6969032
Anthanh <antai.pt@gmail.com>
2024-06-04 13:09:58 +0200

    feat: add object helpers

---
+++ src/lib/object.test.ts
@@ -0,0 +1,42 @@
+import { isObjectEmpty, removeEmptyOrNull } from "./object";
+
+describe("removeEmptyOrNull", () => {
+  it("removes empty or null properties from an object", () => {
+    const obj = {
+      name: "John",
+      age: null,
+      address: {
+        street: "",
+        city: "New York",
+        state: null,
+      },
+    };
+
+    const expected = {
+      name: "John",
+      address: {
+        city: "New York",
+      },
+    };
+
+    const actual = removeEmptyOrNull(obj);
+    expect(actual).toStrictEqual(expected);
+  });
+});
+
+describe("isObjectEmpty", () => {
+  test("returns true if the object is empty", () => {
+    const obj = {};
+    expect(isObjectEmpty(obj)).toBe(true);
+  });
+
+  test("returns false if the object is not empty", () => {
+    const obj = { name: "John" };
+    expect(isObjectEmpty(obj)).toBe(false);
+  });
+
+  test("returns true if the object is null or undefined", () => {
+    expect(isObjectEmpty(null)).toBe(true);
+    expect(isObjectEmpty(undefined)).toBe(true);
+  });
+});
---
+++ src/lib/object.ts
@@ -0,0 +1,31 @@
+export const removeEmptyOrNull = (obj: Record<string, unknown>) => {
+  for (const clave in obj) {
+    const value = obj[clave];
+
+    if (typeof value === "object" && !(value instanceof Array)) {
+      removeEmptyOrNull(value as Record<string, unknown>);
+    }
+
+    if (
+      value === null ||
+      value === undefined ||
+      value === "" ||
+      value === 0 ||
+      (Array.isArray(value) && value.length === 0)
+    ) {
+      delete obj[clave];
+    }
+  }
+
+  return obj;
+};
+
+export function isNullish(value: unknown): value is null | undefined {
+  return value === null || value === undefined;
+}
+
+export function isObjectEmpty(obj: Record<string, unknown> | null | undefined): boolean {
+  if (!obj) return true;
+
+  return Object.keys(obj).length === 0;
+}
