0c2955e85794936fe4c60cb201a3dc5882175d3d
Anthanh <antai.pt@gmail.com>
2025-06-12 15:34:26 +0200

    style: minor change

--- src/i18n/components/LocaleSelector.tsx
+++ src/i18n/components/LocaleSelector.tsx
@@ -22,7 +22,7 @@ export function LocaleSelector() {
     <select
       value={language}
       onChange={handleLanguageChange}
-      className="px-2 py-1 text-sm border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      className="px-2 py-1 text-sm text-black border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
     >
       {Object.values(Locale).map((locale) => (
         <option key={locale} value={locale}>
